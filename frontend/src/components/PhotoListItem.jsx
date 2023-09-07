5e13c9cf46dd6b2ff69210d809648ac6757eafdd
Difan <dfhou29@gmail.com>
2023-09-06 22:27:18 -0700

    add style for photo list item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,17 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   /* Insert React */
   return (
-    <div key={props.key}>
-      <img src={props.image} alt="photo"/>
-      <img src={props.profile} alt="profile picture"/>
-      <p>{props.username}</p>
-      <p>{props.city}, {props.country}</p>
+    <div key={props.key} className="photo-list__item">
+      <img src={props.image} alt="photo" className="photo-list__image"/>
+
+      <div className="photo-list__user-details">
+        <img src={props.profile} alt="profile picture" className="photo-list__user-profile"/>
+        <div className="photo-list__user-info">
+          <p>{props.username}</p>
+          <p className="photo-list__user-location">{props.city}, {props.country}</p>
+        </div>
+      </div>
+
     </div>
 
 
