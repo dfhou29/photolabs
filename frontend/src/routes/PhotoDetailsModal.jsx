ed81c48c9c1032c4d3c671c50cd6ad518923800a
Difan <dfhou29@gmail.com>
2023-09-10 21:05:39 -0700

    update hook

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
-import photos from "../mocks/photos";
 import PhotoList from "../components/PhotoList";
 import PhotoFavButton from "../components/PhotoFavButton";
 
@@ -10,33 +9,37 @@ const PhotoDetailsModal = ({detailModal, setDetailModal, isSelected, detailItem,
 
 
   const similarPhotos = [];
-  console.log(detailItem.id);
+  console.log(detailItem);
   for (const photo in detailItem['similar_photos']) {
     similarPhotos.push(detailItem['similar_photos'][photo]);
   }
 
 
-
-
-
-  return (<div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" onClick={() => setDetailModal(false)}/>
-      </button>
-      <div className="photo-details-modal__images">
-        {/*<PhotoList photos={[detailItem]} favorite={favorite} setFavorite={setFavorite} detailModal={detailModal} setDetailModal={setDetailModal} detailItem={detailItem} setDetailItem={setDetailItem}/>*/}
-        <div>
-          <PhotoFavButton selected={isSelected(detailItem.id)} onClick={()=>handleLike(detailItem)}/>
-          <img src={detailItem.urls.full} alt="profile picture" className="photo-details-modal__image"/>
-          {/*<div className="photo-details-modal__photographer-details">*/}
-          {/*  <p>{detailItem.user.name}</p>*/}
-          {/*  <p>{detailItem.location.city}, {detailItem.location.country}</p>*/}
-          {/*</div>*/}
-        </div>
+  return (
+    <div className="photo-details-modal">
+      <div className="photo-details-modal__top-bar">
+        <button className="photo-details-modal__close-button">
+          <img src={closeSymbol} alt="close symbol" onClick={() => setDetailModal(false)}/>
+        </button>
       </div>
+
       <div className="photo-details-modal__images">
-        <PhotoList photos={similarPhotos} detailModal={detailModal}
-                   setDetailModal={setDetailModal} detailItem={detailItem} setDetailItem={setDetailItem} isSelected={isSelected} handleLike={handleLike}/>
+        <PhotoFavButton selected={isSelected(detailItem.id)} onClick={() => handleLike(detailItem)}/>
+        <img src={detailItem.urls.full} alt="profile picture" className="photo-details-modal__image"/>
+        <div className="photo-list__user-details">
+          <img src={detailItem.user.profile} alt="profile picture" className="photo-list__user-profile"/>
+          <div className="photo-list__user-info">
+            <p>{detailItem.user.name}</p>
+            <p className="photo-list__user-location">{detailItem.location.city}, {detailItem.location.country}</p>
+          </div>
+        </div>
+        <h3 className="photo-details-modal__header">Similar Photos</h3>
+        <div>
+          <PhotoList photos={similarPhotos} detailModal={detailModal}
+                     setDetailModal={setDetailModal} detailItem={detailItem} setDetailItem={setDetailItem}
+                     isSelected={isSelected} handleLike={handleLike}/>
+        </div>
+
 
       </div>
     </div>)
